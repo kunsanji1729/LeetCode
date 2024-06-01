e8d6bc09e2520edf07dc9a39c5027dee9ba557ad
kunsanji1729 <kunsanji1729@gmail.com>
2024-06-01 11:41:21 +0530

    :ballot_box_with_check: isomorphic-strings [Runtime: 0 ms; Memory: 8.2 MB]

---
+++ string/isomorphic-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int hash[256] = {0};
+        bool istCharMapped[256]={0};
+
+        for(int i=0;i<s.length();i++){
+            if(hash[s[i]] == 0 && istCharMapped[t[i]] == 0){
+                hash[s[i]] = t[i];
+                istCharMapped[t[i]]=true;
+            }
+        }
+
+        for(int i=0;i<s.length();i++){
+            if(hash[s[i]] != t[i]){
+                return false;
+            }
+        }
+
+        return true;
+        
+    }
+};
