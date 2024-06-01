97aee77faa7be4356814352fe1e7cc252ff36b41
kunsanji1729 <kunsanji1729@gmail.com>
2024-06-01 11:43:29 +0530

    :ballot_box_with_check: isomorphic-strings [Runtime: 165 ms; Memory: 9.5 MB]

--- string/isomorphic-strings.cpp
+++ string/isomorphic-strings.cpp
@@ -1,23 +1,70 @@
 class Solution {
 public:
-    bool isIsomorphic(string s, string t) {
-        int hash[256] = {0};
-        bool istCharMapped[256]={0};
-
-        for(int i=0;i<s.length();i++){
-            if(hash[s[i]] == 0 && istCharMapped[t[i]] == 0){
-                hash[s[i]] = t[i];
-                istCharMapped[t[i]]=true;
-            }
-        }
-
-        for(int i=0;i<s.length();i++){
-            if(hash[s[i]] != t[i]){
+    bool is_digits(string& str) {
+        for (char ch : str) {
+            int v = ch; // ASCII Val converted
+            if (!(ch >= 48 && ch <= 57)) {
                 return false;
             }
         }
 
         return true;
-        
+    }
+    bool isChar(char ch) {
+        if (ch >= '0' && ch <= '9') {
+            return false;
+        }
+        return true;
+    }
+    string replaceWithNum(string s) {
+        int num = 1;
+        for (int i = 0; i < s.length(); i++) {
+            char temp = s[i];
+
+            if (isChar(s[i])) {
+                // replace with num all occurance
+                for (int j = 0; j < s.length(); j++) {
+                    if (s[j] == temp) {
+                        s.replace(j, 1, to_string(num));
+                    }
+                }
+                num++;
+            }
+        }
+        return s;
+    }
+    string replaceWithChar(string s) {
+        char ch = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char temp = s[i];
+
+            if (!isChar(s[i])) {
+                for (int j = i; j < s.length(); j++) {
+                    if (s[j] == temp) {
+                        s[j] = ch;
+                    }
+                }
+                ch++;
+            }
+        }
+        return s;
+    }
+    bool isIsomorphic(string s, string t) {
+        if (is_digits(s) && is_digits(t)) {
+            s = replaceWithChar(s);
+            t = replaceWithChar(t);
+
+            if (s == t) {
+                return true;
+            }
+            return false;
+        }
+        s = replaceWithNum(s);
+        t = replaceWithNum(t);
+
+        if (s == t) {
+            return true;
+        }
+        return false;
     }
 };
